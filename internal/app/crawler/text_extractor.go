8e60475607ffd7d709e96deb3701291425782c30
samwang-crypto <sam.wang@crypto.com>
2022-08-14 00:41:47 +0800

    prepare for stake concentration ETL extraction

--- internal/app/crawler/text_extractor.go
+++ internal/app/crawler/text_extractor.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,21 +15,26 @@ package crawler
 
 import (
 	"context"
-	"fmt"
+	"strconv"
 
 	"github.com/samwang0723/stock-crawler/internal/app/entity"
+	"github.com/samwang0723/stock-crawler/internal/app/entity/convert"
 	"github.com/samwang0723/stock-crawler/internal/app/parser"
 	"github.com/samwang0723/stock-crawler/internal/app/pipeline"
+
+	"github.com/sirupsen/logrus"
 )
 
 type textExtractor struct {
 	parser   parser.Parser
+	logger   *logrus.Entry
 	memCache map[string][]*entity.StakeConcentration
 }
 
-func newTextExtractor(parser parser.Parser) *textExtractor {
+func newTextExtractor(parser parser.Parser, logger *logrus.Entry) *textExtractor {
 	return &textExtractor{
 		parser:   parser,
+		logger:   logger,
 		memCache: make(map[string][]*entity.StakeConcentration),
 	}
 }
@@ -37,19 +42,53 @@ func newTextExtractor(parser parser.Parser) *textExtractor {
 func (te *textExtractor) Process(ctx context.Context, p pipeline.Payload) (pipeline.Payload, error) {
 	payload := p.(*crawlerPayload)
 	te.parser.SetStrategy(payload.Strategy, payload.Date)
-	// Bypass the parsing error
-	te.parser.Execute(payload.RawContent, payload.URL)
+	err := te.parser.Execute(payload.RawContent, payload.URL)
+	if err != nil {
+		return nil, err
+	}
 
-	//TODO: do switch case to parse different types
-	objs := te.parser.Flush()
-	for _, v := range *objs {
-		if val, ok := v.(*entity.StakeConcentration); ok {
-			te.memCache[val.StockID] = append(te.memCache[val.StockID], val)
-			fmt.Printf("Count: %d, Process: %+v\n", len(te.memCache[val.StockID]), val)
-			if len(te.memCache[val.StockID]) == 5 {
-				//TODO: send through Kafka channel
+	err = te.broadcast(ctx, payload.Strategy, te.parser.Flush())
+	if err != nil {
+		return nil, err
+	}
+
+	return p, nil
+}
+
+func (te *textExtractor) broadcast(ctx context.Context, strategy convert.Source, data *[]interface{}) error {
+	switch strategy {
+	case convert.TwseStockList, convert.TpexStockList:
+	case convert.TwseDailyClose, convert.TpexDailyClose:
+	case convert.TwseThreePrimary, convert.TpexThreePrimary:
+	case convert.StakeConcentration:
+		for _, v := range *data {
+			if val, ok := v.(*entity.StakeConcentration); ok {
+				te.memCache[val.StockID] = append(te.memCache[val.StockID], val)
+				if len(te.memCache[val.StockID]) == 5 {
+					te.logger.Infof("Count: %d, Process: %+v", len(te.memCache[val.StockID]), te.mapReduce(ctx, te.memCache[val.StockID]))
+					//TODO: clear in-memory cache
+				}
 			}
 		}
 	}
-	return p, nil
+
+	return nil
+}
+
+func (te *textExtractor) mapReduce(ctx context.Context, objs []*entity.StakeConcentration) *entity.StakeConcentration {
+	volumeDiff := []int32{0, 0, 0, 0, 0}
+	var res *entity.StakeConcentration
+
+	for _, val := range objs {
+		idx, _ := strconv.Atoi(val.HiddenField)
+		// make sure to cover latest concentration data
+		if idx == 0 {
+			res = val
+		}
+		volumeDiff[idx] = int32(val.SumBuyShares - val.SumSellShares)
+	}
+
+	res.Diff = volumeDiff
+
+	return res
 }
--- internal/app/handlers/downloader.go
+++ internal/app/handlers/downloader.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -44,6 +44,7 @@ func (h *handlerImpl) Download(ctx context.Context, req *dto.StartCronjobRequest
 // batching download all the historical stock data
 func (h *handlerImpl) batchingDownload(ctx context.Context, rewind int32, types []convert.Source) {
 	var links []*graph.Link
+
 	for _, t := range types {
 		d := formatQueryDate(rewind, t)
 		urls := h.generateURLs(ctx, d, t)
